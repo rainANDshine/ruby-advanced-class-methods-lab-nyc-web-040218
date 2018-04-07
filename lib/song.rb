b9063376eaa7536388e9781bb796254e83fedbbf
Shun Yao <ysmiracle@hotmail.com>
2018-04-07 07:56:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    if self.all.any? {|song| song.name == name}
+    if self.find_by_name(name) != nil
       self.find_by_name(name)
     else
       self.create_by_name(name)
